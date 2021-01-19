bd8769f9bafb4f0c13a40d9ea851251aa616ebb4
jempico <jpfilarca@gmail.com>
2021-01-19 21:33:39 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,68 +1,59 @@
 /*SELECTING DINAMIC ELEMENTS*/
 
 const calculator = document.querySelector('.calculator');
-const display = document.querySelector('.result');
+const result = calculator.querySelector('.result');
 const keys = calculator.querySelector('.keys');
 
 /*ADDING EVENT LISTENERS*/
 
 keys.addEventListener('click', calculate);
 
-
-/* DEFINING CONSTANTS IN THE LOCAL SCOPE OF THE CALCULATE FUNCTION*/
+/* DEFINING FUNCTIONS */
 
 function calculate(e){
-    //Grabbing button that has been clicked.
     const key = e.target
-
-    //Grabbing inner HTML content of the target button.
     const keyContent = key.textContent;
-
-    //Grabbing displayed number from the result div.
+    const display = document.querySelector('.result');
     const displayedNum = display.textContent;
-
-    // Using the data-action attribute to grab the type of operator associated with the button thas has been clicked (stored in key-opertor button). 
     const action = key.dataset.action; 
-
-    // Grabbing the type of key that has been cliked before (data-attribute stored in calculator div);
     const previousKeyType = calculator.dataset.previousKeyType;
 
 
 /*STATEMENTS*/
 
-    // If the element doesn't have a data-action attribute it must be a number.
+    // If the element doesn't have a data-attribute it must be a number.
     if (!action) {
-        if (displayedNum === '0' || previousKeyType === 'operator') {
-            display.textContent = keyContent; //> Replacing 0 for clicked number.
-        } else {
-            display.textContent = displayedNum + keyContent; //> Concadenating numbers.
+
+      if (displayedNum === '0') {
+          display.textContent = keyContent;
+      
+      //Displaying second value in the result screen, after an operator button has been clicked.
+      } else if (previousKeyType === 'operator') {
+          display.textContent = keyContent;
+          calculator.dataset.previousKeyType = '';
+      }
+      //> Concadenating numbers.
+        else {
+            display.textContent = displayedNum + keyContent; 
         }
     }
     if (action == 'decimal') {
         display.textContent = displayedNum + "."; //>Concadenating decimals.
     }
-    if (action == 'percentage' ||
-        action == 'divide' ||
-        action == 'add' ||
-        action == 'substract' ||
-        action == 'multiply' 
-        ) {
-        key.classList.add('is-clicked');
-        
-        //↓ Adding 3 custom data attributes to calculator in order to grab three different values that will be updated every time a user clicks on an operator key.
-        
-        // Grabbing the type of key that has been clicked in order to tell if an operator key has been clicked.
-        calculator.dataset.previousKeyType = 'operator';
 
-        //Same process to grab the number that was displayed previously.
-        calculator.dataset.firstValue = displayedNum;
-        
-        //Same process to grab the operator clicked.
-        calculator.dataset.operator = action;
+    if (key.classList[0] === "key-operator") {
+      
+      // Grabbing the type of key that has been clicked in order to tell if an operator key has been clicked.
+      calculator.dataset.previousKeyType = 'operator';
+      calculator.dataset.firstValue = displayedNum;
+      calculator.dataset.operator = action;
+    
+      if (action === 'percentage') {
+        display.textContent = parseFloat(displayedNum) / 100;
+      }
+
     }
- 
 
-    //When a user clicks the equal sign we trigger the make Operation function with 3 parameters.
     if (action === 'calculate') {
         const firstValue = calculator.dataset.firstValue;
         const operator = calculator.dataset.operator;
@@ -71,26 +62,34 @@ function calculate(e){
         display.textContent = makeOperation(firstValue, operator, secondValue);
     }
 
+    if (action === 'clear') {
+      display.textContent = 0;
+    }
+
+    if (action === 'sign') {
+      if (displayedNum > 0) {
+      display.textContent = "-" + displayedNum;
+      } 
+      if ((displayedNum < 0)) {
+        display.textContent =  displayedNum.slice(1, displayedNum.length);
+      }
+      }
+    
 
 /* MAKE OPERATION FUNCTION */
 
 function makeOperation(n1, operator, n2) {
-        let result = ''
         let parsed1 = parseFloat(n1);
         let parsed2 = parseFloat(n2);
 
         if (operator === 'add') {
-          result = parsed1 + parsed2
+          return parsed1 + parsed2
         } else if (operator === 'substract') {
-          result = parsed1 - parsed2
+          return parsed1 - parsed2
         } else if (operator === 'multiply') {
-          result = parsed1 * parsed2
+          return parsed1 * parsed2
         } else if (operator === 'divide') {
-          result = parsed1 / parsed2
-        } else if (operator == 'percentage') {
-            result = parsed1 / 1000;
+          return parsed1 / parsed2
         }
-        
-        return result;
       }
-}
+    }
